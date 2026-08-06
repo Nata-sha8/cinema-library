f24f236e4f412235ebaaad16d33eb28ac70e1089
Nata-sha8 <nikitinalogist@gmail.com>
2026-08-06 14:16:00 +0300

    split chunks for better loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/",
   build: {
     outDir: "dist",
-  },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Разбиваем по названиям модулей
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'reactVendor';
+            }
+            if (id.includes('react-router-dom')) {
+              return 'router';
+            }
+            if (id.includes('swiper')) {
+              return 'swiper';
+            }
+            // Все остальное из node_modules
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 });
